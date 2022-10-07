7ac729f40464218080af829763e01cdcd3c148df
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2022-10-07 04:43:38 +0300

    feat(Card): add functionality to the card to transist from the innitial state to the thanks state

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // This page is meant to handle the functionality of the Rating Component.
+
+const cardRateState = document.getElementById("state-rate");
+const cardThanksState = document.getElementById("state-thanks");
+const btnSubmit = document.getElementById("btn-submit");
+const btns = document.querySelectorAll(".card__ratings_rate");
+const rate = document.getElementById("rate");
+
+const handleSubmit = () => {
+  cardRateState.classList.add("hidden");
+  cardThanksState.classList.remove("hidden");
+};
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    rate.innerHTML = btn.innerHTML;
+  });
+});
